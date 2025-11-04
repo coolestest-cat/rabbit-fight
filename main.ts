cdd018afbaab039ac5bf70263f6bfe37931ac8c6
bluecheshirecat <ivarghese6406@colliervilledragons.org>
2025-11-04 07:47:50 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,253 @@
+namespace SpriteKind {
+    export const player1 = SpriteKind.create()
+}
 function move_sprite () {
+    player1 = sprites.create(img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f f . . . . . 
+        . . . . f 1 5 2 5 1 6 f . . . . 
+        . . . f 1 6 6 6 6 6 1 6 f . . . 
+        . . . f 6 6 f f f f 6 1 f . . . 
+        . . . f 6 f f d d f f 6 f . . . 
+        . . f 6 f d f d d f d f 6 f . . 
+        . . f 6 f d 3 d d 3 d f 6 f . . 
+        . . f 6 6 f d d d d f 6 6 f . . 
+        . f 6 6 f 3 f f f f 3 f 6 6 f . 
+        . . f f d 3 5 3 3 5 3 d f f . . 
+        . . f d d f 3 5 5 3 f d d f . . 
+        . . . f f 3 3 3 3 3 3 f f . . . 
+        . . . f 3 3 5 3 3 5 3 3 f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 100, 100)
     characterAnimations.loopFrames(
     player1,
     [img`
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . f 5 5 5 5 f . . . . . 
+        . . . . f 6 6 6 6 6 6 f . . . . 
+        . . . f 6 1 1 1 6 1 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . f f 6 6 6 6 6 6 6 6 f f . . 
+        . f 6 6 6 f 6 6 6 6 f 6 6 6 f . 
+        . . f f f 3 f f f f 3 f f f . . 
+        . . . f d 5 3 3 3 3 5 d f . . . 
+        . . f d d f 3 3 3 3 f d d f . . 
+        . . . f f f 5 3 3 5 f f f . . . 
+        . . . . f 3 3 5 5 3 3 f . . . . 
+        . . . . f 3 3 3 3 3 3 f . . . . 
+        . . . . . f f f f f f . . . . . 
+        `,img`
         . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . f 5 5 5 5 f . . . . . 
+        . . . . f 6 6 6 6 6 6 f . . . . 
+        . . . f 6 1 1 1 6 1 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . f f 6 6 6 6 6 6 6 6 f f . . 
+        . f 6 6 6 f 6 6 6 6 f 6 6 6 f . 
+        . . f f f 3 f f f f 5 f f f . . 
+        . . . f d f 3 3 3 3 d d f . . . 
+        . . . . f 3 5 3 3 f d d f . . . 
+        . . . . f 3 3 5 5 3 f f . . . . 
+        . . . . f f 3 3 f f . . . . . . 
+        . . . . . . f f . . . . . . . . 
+        `,img`
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . f 5 5 5 5 f . . . . . 
+        . . . . f 6 6 6 6 6 6 f . . . . 
+        . . . f 6 1 1 1 6 1 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . f f 6 6 6 6 6 6 6 6 f f . . 
+        . f 6 6 6 f 6 6 6 6 f 6 6 6 f . 
+        . . f f f 5 f f f f 3 f f f . . 
+        . . . f d d 3 3 3 3 f d f . . . 
+        . . . f d d f 3 3 5 3 f . . . . 
+        . . . . f f 3 5 5 3 3 f . . . . 
+        . . . . . . f f 3 3 f f . . . . 
+        . . . . . . . . f f . . . . . . 
         `],
-    500,
-    characterAnimations.rule(Predicate.Moving, Predicate.NotMoving)
+    100,
+    characterAnimations.rule(Predicate.MovingDown)
+    )
+    characterAnimations.loopFrames(
+    player1,
+    [img`
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . f 5 5 5 5 f . . . . . 
+        . . . . f 6 6 6 6 6 6 f . . . . 
+        . . . f 6 1 1 1 6 1 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . f f 6 6 6 6 6 6 6 6 f f . . 
+        . f 6 6 6 f 6 6 6 6 f 6 6 6 f . 
+        . . f f f 3 f f f f 3 f f f . . 
+        . . . f d 5 3 3 3 3 5 d f . . . 
+        . . f d d f 3 3 3 3 f d d f . . 
+        . . . f f f 5 3 3 5 f f f . . . 
+        . . . . f 3 3 5 5 3 3 f . . . . 
+        . . . . f 3 3 3 3 3 3 f . . . . 
+        . . . . . f f f f f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . f 5 5 5 5 f . . . . . 
+        . . . . f 6 6 6 6 6 6 f . . . . 
+        . . . f 6 1 1 1 6 1 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . f f 6 6 6 6 6 6 6 6 f f . . 
+        . f 6 6 6 f 6 6 6 6 f 6 6 6 f . 
+        . . f f f 3 f f f f 5 f f f . . 
+        . . . f d f 3 3 3 3 d d f . . . 
+        . . . . f 3 5 3 3 f d d f . . . 
+        . . . . f 3 3 5 5 3 f f . . . . 
+        . . . . f f 3 3 f f . . . . . . 
+        . . . . . . f f . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . f 5 5 5 5 f . . . . . 
+        . . . . f 6 6 6 6 6 6 f . . . . 
+        . . . f 6 1 1 1 6 1 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . f f 6 6 6 6 6 6 6 6 f f . . 
+        . f 6 6 6 f 6 6 6 6 f 6 6 6 f . 
+        . . f f f 5 f f f f 3 f f f . . 
+        . . . f d d 3 3 3 3 f d f . . . 
+        . . . f d d f 3 3 5 3 f . . . . 
+        . . . . f f 3 5 5 3 3 f . . . . 
+        . . . . . . f f 3 3 f f . . . . 
+        . . . . . . . . f f . . . . . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+    characterAnimations.loopFrames(
+    player1,
+    [img`
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 5 5 2 6 f . . . . 
+        . . . . f 6 6 1 6 6 6 6 f . . . 
+        . . . . f 6 1 6 6 6 6 6 f . . . 
+        . . . . f 1 6 6 6 d f d f . . . 
+        . . . f f 6 6 6 6 d f d f . . . 
+        . . f 6 f 6 6 6 d d 3 d f . . . 
+        . . . f f 6 f f d d d f . . . . 
+        . . f 6 6 6 f 3 5 f f . . . . . 
+        . . . f f f f f 3 3 5 f . . . . 
+        . . . . . . f d f 3 3 f . . . . 
+        . . . . . . f d f 3 f . . . . . 
+        . . . . . f d f 3 5 3 f . . . . 
+        . . . . . . f f 3 3 f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 5 5 2 6 f . . . . . 
+        . . . f 6 6 1 6 6 6 6 f . . . . 
+        . . . f 6 1 6 6 6 6 6 f . . . . 
+        . . . f 1 6 6 6 d f d f . . . . 
+        . . f f 6 6 6 6 d f d f . . . . 
+        . f 6 f 6 6 6 d d 3 d f . . . . 
+        . . f f 6 f f d d d f . . . . . 
+        . f 6 6 f f 3 3 f f . . . . . . 
+        . . f f f f d d d d f . . . . . 
+        . . . . f 3 f d d d f . . . . . 
+        . . . f 3 5 d f f f . . . . . . 
+        . . . . f f 3 3 f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 5 5 2 6 f . . . . . 
+        . . . f 6 6 1 6 6 6 6 f . . . . 
+        . . . f 6 1 6 6 6 6 6 f . . . . 
+        . . . f 1 6 6 6 d f d f . . . . 
+        . . f f 6 6 6 6 d f d f . . . . 
+        . f 6 f 6 6 6 d d 3 d f . . . . 
+        . . f f 6 f f d d d f . . . . . 
+        . f 6 6 f f 3 3 f f . . . . . . 
+        . . f f f d d 3 3 5 f . . . . . 
+        . . . f d d f 3 3 3 f . . . . . 
+        . . . . f f f 5 3 f . . . . . . 
+        . . . . . f 3 3 3 3 f . . . . . 
+        . . . . . f f f f f . . . . . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingRight)
+    )
+    characterAnimations.loopFrames(
+    player1,
+    [img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 2 5 5 6 f . . . . . 
+        . . . f 6 6 6 6 1 6 6 f . . . . 
+        . . . f 6 6 6 6 6 1 6 f . . . . 
+        . . . f d f d 6 6 6 1 f . . . . 
+        . . . f d f d 6 6 6 6 f f . . . 
+        . . . f d 3 d d 6 6 6 f 6 f . . 
+        . . . . f d d d f f 6 f f . . . 
+        . . . . . f f 5 3 f 6 6 6 f . . 
+        . . . . f 5 3 3 f f f f f . . . 
+        . . . . f 3 3 f d f . . . . . . 
+        . . . . . f 3 f d f . . . . . . 
+        . . . . f 3 5 3 f d f . . . . . 
+        . . . . f f 3 3 f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 2 5 5 6 f . . . . 
+        . . . . f 6 6 6 6 1 6 6 f . . . 
+        . . . . f 6 6 6 6 6 1 6 f . . . 
+        . . . . f d f d 6 6 6 1 f . . . 
+        . . . . f d f d 6 6 6 6 f f . . 
+        . . . . f d 3 d d 6 6 6 f 6 f . 
+        . . . . . f d d d f f 6 f f . . 
+        . . . . . . f f 3 3 f f 6 6 f . 
+        . . . . . f d d d d f f f f . . 
+        . . . . . f d d d f 3 f . . . . 
+        . . . . . . f f f d 5 3 f . . . 
+        . . . . . f f f 3 3 f f . . . . 
+        . . . . . f f f f f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 2 5 5 6 f . . . . 
+        . . . . f 6 6 6 6 1 6 6 f . . . 
+        . . . . f 6 6 6 6 6 1 6 f . . . 
+        . . . . f d f d 6 6 6 1 f . . . 
+        . . . . f d f d 6 6 6 6 f f . . 
+        . . . . f d 3 d d 6 6 6 f 6 f . 
+        . . . . . f d d d f f 6 f f . . 
+        . . . . . . f f 3 3 f f 6 6 f . 
+        . . . . . f 5 3 3 d d f f f . . 
+        . . . . . f 3 3 3 f d d f . . . 
+        . . . . . . f 3 5 f f f . . . . 
+        . . . . . f 3 3 3 3 f . . . . . 
+        . . . . . . f f f f f . . . . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingLeft)
     )
 }
 let player1: Sprite = null
@@ -277,6 +504,7 @@ scene.setBackgroundImage(img`
     111111111111111111111111111111111111111111111111111111111111111111111999996666999999999999999999999999999999999999999999999999999999999999999999999999
     `)
 tiles.setCurrentTilemap(tilemap`level1`)
+move_sprite()
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), player1)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
     . . . . . . . . . . . . . . . . 
